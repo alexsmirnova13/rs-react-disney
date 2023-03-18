349f700cbe77832826f1c773087a43e9d9edfa8e
alexsmirnova13 <alexandrasmirnovae@gmail.com>
2023-03-18 16:51:51 +0300

    init: first init+config

---
+++ disney-app/webpack.config.js
@@ -0,0 +1,34 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+};
