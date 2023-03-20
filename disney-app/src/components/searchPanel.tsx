599a6eaa3ccff84520fba8dccdbaf0465e1220b0
alexsmirnova13 <alexandrasmirnovae@gmail.com>
2023-03-20 03:13:57 +0300

    feat: input task

--- disney-app/src/components/searchPanel.tsx
+++ disney-app/src/components/searchPanel.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import styled from '@emotion/styled';
-const StyledSearchBar = styled.div``;
+const StyledSearchBar = styled.form``;
 class SearchPanel extends React.Component {
+  state = {
+    inputValue: '',
+  };
+
+  handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    const { inputValue } = this.state;
+    const key = new Date().toISOString();
+    localStorage.setItem(key, inputValue);
+  };
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const inputValue = event.target.value;
+    this.setState({ inputValue });
+  };
   render() {
+    const { inputValue } = this.state;
     return (
-      <StyledSearchBar data-testid="search">
-        <input data-testid="input" />
-        <button data-testid="button">Search</button>
+      <StyledSearchBar data-testid="search" onSubmit={this.handleSubmit}>
+        <input data-testid="input" value={inputValue} onChange={this.handleInputChange} />
+        <button data-testid="button" type="submit">
+          Search
+        </button>
       </StyledSearchBar>
     );
   }
