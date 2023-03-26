21438857073da06ef7e4c1cdb0d8e4646fdecf11
alexsmirnova13 <alexandrasmirnovae@gmail.com>
2023-03-27 00:41:48 +0300

    feat: design notification

--- disney-app/src/components/notification.tsx
+++ disney-app/src/components/notification.tsx
@@ -3,6 +3,13 @@ import styled from '@emotion/styled';
 
 const StyledMessage = styled.div`
   position: absolute;
+  color: green;
+  display: block;
+  border-radius: 8px;
+  border: 1px solid green;
+  top: 150px;
+  left: 30px;
+  padding: 6px 20px 4px 10px;
 `;
 interface IMessageProps {
   message: string;
